d299de572b81e064160b8acabbba7330132b2a3b
Emil Albin Ryberg <emil.a.ryberg@gmail.com>
2018-04-30 11:14:43 +0200

    Made QT code a bit less naughty

--- QT Interface/QTInterface/mainwindow.cpp
+++ QT Interface/QTInterface/mainwindow.cpp
@@ -1,11 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "serialportreader.h"
+#include <QSerialPort>
+#include <QStringList>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QTextStream standardOutput(stdout);
+    QSerialPort serialPort;
+    const QString serialPortName = "COM3";
+
+    serialPort.setPortName(serialPortName);
+    serialPort.setBaudRate(115200);
+    if (!serialPort.open(QIODevice::ReadOnly)) {
+        standardOutput << QObject::tr("Failed to open port %1, error: %2")
+                          .arg(serialPortName)
+                          .arg(serialPort.errorString())
+                       << endl;
+    }
+
+    SerialPortReader serialPortReader(&serialPort, ui);
 }
 
 MainWindow::~MainWindow()
