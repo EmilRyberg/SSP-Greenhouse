0ae032213c9fd8e23d6f774e3d3d7bad06325c2e
Albert S. Olesen <asol17@student.aau.dk>
2018-05-04 10:04:55 +0200

    added sensor header and source

---
+++ QT Interface/QTInterface/sensor.cpp
@@ -0,0 +1,8 @@
+#include "sensor.h"
+#include <iomanip>      // std::setprecision
+#include <QCoreApplication>
+#include <stdlib.h>     /* atof */
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
---
+++ QT Interface/QTInterface/sensor.h
@@ -0,0 +1,19 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <QByteArray>
+#include <QTextStream>
+#include <vector>
+
+class sensor{
+    public:
+        double time;
+        std::vector<double> data;
+
+    private:
+        double getAverage(getAverage){
+
+        };
+};
+
+#endif // SENSOR_H
